5857cb94113c27c3723385db5128ae220af8871c
qt <saki.log@qq.com>
2024-09-22 00:46:00 +0800

    player attack combo timer

--- Scripts/Characters/Player/PlayerAttackState.cs
+++ Scripts/Characters/Player/PlayerAttackState.cs
@@ -5,12 +5,24 @@ namespace DungeonRpg.Scripts.Characters.Player;
 
 public partial class PlayerAttackState : PlayerState
 {
+    [Export] private Timer _attackTimer;
     private int _comboCounter = 1;
     private int _maxComboCount = 2;
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _attackTimer.Timeout += HandleTimeout;
+    }
+
     protected override void EnterState()
     {
-        CharacterNode.AnimPlayerNode.Play(GameConstants.AnimAttack + _comboCounter);
+        CharacterNode.AnimPlayerNode.Play(
+            GameConstants.AnimAttack + _comboCounter,
+            -1,
+            1.5f
+        );
 
         CharacterNode.AnimPlayerNode.AnimationFinished += HandleAnimationFinished;
     }
@@ -18,13 +30,20 @@ public partial class PlayerAttackState : PlayerState
     protected override void ExitState()
     {
         CharacterNode.AnimPlayerNode.AnimationFinished -= HandleAnimationFinished;
+
+        _attackTimer.Start();
     }
 
     private void HandleAnimationFinished(StringName animName)
     {
         _comboCounter++;
         _comboCounter = Mathf.Wrap(_comboCounter, 1, _maxComboCount + 1);
-        
+
         CharacterNode.StateMachineNode.SwitchState<PlayerIdleState>();
+    }
+
+    private void HandleTimeout()
+    {
+        _comboCounter = 1;
     }
 }
